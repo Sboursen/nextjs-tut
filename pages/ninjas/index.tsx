9993e1fee781cdc83a40842ad48c515149a61f57
Soufiane Boursen <dev.sboursen@gmail.com>
2022-08-25 22:05:26 +0100

    Create Ninjas resource

---
+++ pages/ninjas/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Ninjas: NextPage = () => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+    </div>
+  );
+};
+
+export default Ninjas;
---
+++ pages/ninjas/test.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Test: NextPage = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default Test;
