9c0e4e447a2e534173e17dd392c8147780b4a4ae
Soufiane Boursen <dev.sboursen@gmail.com>
2022-08-25 21:56:59 +0100

    Add About page

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        necessitatibus eos facere, voluptatibus est illo totam ad tempore
+        suscipit perspiciatis eligendi veritatis dolores animi deleniti expedita
+        ipsum non magni commodi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        necessitatibus eos facere, voluptatibus est illo totam ad tempore
+        suscipit perspiciatis eligendi veritatis dolores animi deleniti expedita
+        ipsum non magni commodi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
