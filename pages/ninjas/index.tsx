e95c01c4f40baf25c66c62aa9388f04d92352722
Soufiane Boursen <dev.sboursen@gmail.com>
2022-08-29 16:48:15 +0100

    Fetch user data using getStaticProps

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,9 +1,41 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
 
-const Ninjas: NextPage = () => {
+type Primitive = boolean | number | string | null;
+type JSONArray = JSONValue[];
+type JSONObject = { [key: string]: JSONValue };
+type JSONValue = JSONObject | JSONArray | Primitive;
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: JSONObject;
+  phone: string;
+  website: string;
+  company: JSONObject;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await response.json();
+
+  return {
+    props: { users },
+  };
+};
+
+const Ninjas: NextPage = ({
+  users,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      <ul>
+        {users.map((user: User) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
